234ee23023ffe477b0c14503cca7b300b85a412f
Felix Miller <felix.miller@hm.edu>
2026-03-04 16:46:31 +0100

    fix: update mailer sender to noreply@circuitverse.ee.hm.edu

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -11,9 +11,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  # TODO: update sender address once email is configured
-  # config.mailer_sender = 'noreply@circuitverse.ee.hm.edu'
-  config.mailer_sender = 'noreply@circuitverse.org'
+  config.mailer_sender = 'noreply@circuitverse.ee.hm.edu'
 
   # Configure the class responsible to send e-mails.
   config.mailer = 'Devise::Mailer'
